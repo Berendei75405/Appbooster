94e8bbd0a519d26bbc264af8f0d0b196faa8b222
Novgorodtcev <73novgor@mail.ru>
2024-11-21 16:27:18 +0700

    Initial Commit

---
+++ Appbooster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appbooster
+//
+//  Created by Novgorodcev on 21/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
